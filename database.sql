9f3c095d84b2779460345440ae7f895195df1603
Krys Frost <librarykrystal@gmail.com>
2024-03-18 20:36:12 -0500

    Updates to database.sql.

--- database.sql
+++ database.sql
@@ -1,6 +1,37 @@
--- Remember USER is a reserved keyword with Postgres, always put in double-quotes in queries!
+-- Remember USER is a reserved keyword with Postgres—
+-- always put "user" in double-quotes in queries!
+
+
+--- [ CREATE TABLES ] ---
+
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "heap" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INT REFERENCES "user",
+    "headline" VARCHAR (100) NOT NULL,
+    "details" VARCHAR (1000),
+    "tag" VARCHAR (1000) REFERENCES "tag",
+);
+
+CREATE TABLE "tag" (
+    "id" SERIAL PRIMARY KEY,
+    "label" VARCHAR (100) NOT NULL,
+    "hex" VARCHAR (10),   -- for color-coding of tags (user chooses color)
+);
+
+
+--- [ INSERT STARTER DATA ] ---
+
+INSERT INTO "tag" ("label", "hex")
+VALUES
+('location', '#fb9062'),
+('physical trait', '#ce4993')
+('personality trait', '#ee5d6c')
+('conflict', '#559e83')
+('dialogue', '#1b85b8')
+('event', '#eeaf61')
