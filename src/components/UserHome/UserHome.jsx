707a6d759343354ad58faa7bae410c296d9076ba
Krys Frost <librarykrystal@gmail.com>
2024-03-22 23:28:43 -0500

    List items on UserHome .map are color-coded by matching tag ID.

--- src/components/UserHome/UserHome.jsx
+++ src/components/UserHome/UserHome.jsx
@@ -9,6 +9,8 @@ import '@fontsource/cabin/400.css';
 import '@fontsource/cabin/700.css';
 import '@fontsource/libre-baskerville';
 import grey from '@mui/material/colors/grey';
+import Button from '@mui/material/Button';
+import WarningIcon from '@mui/icons-material/Warning';
 
 // USER HOME will include:
 // Main view of entire compost heap
@@ -60,21 +62,20 @@ function UserHome() {
   console.log('HEAP:', heap);
   console.log('TAG LIST:', tagList);
 
+
   return (
     <ThemeProvider theme={theme}>
     <div className="container">
-      <h2>Welcome, {user.username}!</h2>
-      <p>Your ID is: {user.id}</p>
-      <p>This is your HEAP!</p>
+      <h3>User: {user.username} / ID: {user.id}</h3>
 
-{/* SHOW TAGS for TESTING*/}
+{/* SHOW TAGS for CONVENIENT TESTING/REFERENCE */}
       {tagList.length >0 &&
         <div className="heap">
           {tagList.map(tag => {
             return (
               <div key={tag.id} >
                 <div className="homeListItem" >
-                  <Typography sx={{ fontSize: 20, fontWeight: 700, mt:2, color: `${tag.hex}`}}>{tag.label}</Typography>
+                  <Typography sx={{ fontWeight: 700, mt:2, color: `${tag.hex}`}}>{tag.label} {tag.hex}</Typography>
                 </div>
               </div>
             );
@@ -83,13 +84,18 @@ function UserHome() {
       }
 
 {/* HEAP LIST */}
-      {heap.length >0 &&
+      {(heap.length >0 && tagList.length >0) &&
         <div className="heap">
           {heap.map(idea => {
+            let tagId = +`${idea.tag_id}`;
+            console.log("TAG ID IN .MAP", tagId);
+            let color = tagList.find(item => item.id == +tagId);
+            console.log("COLOR:", color);
+
             return (
-              <div key={idea.id} >
+              <div key={idea.id}>
                 <div className="homeListItem" >
-                  <Typography sx={{ fontSize: 20, fontWeight: 700, mt:2}}>{idea.headline}</Typography>
+                  <Typography sx={{ fontSize: 20, fontWeight: 700, mt:2, color: `${color.hex}` }} >{idea.headline}</Typography>
                   <Typography>{idea.notes}</Typography>
                 </div>
               </div>
