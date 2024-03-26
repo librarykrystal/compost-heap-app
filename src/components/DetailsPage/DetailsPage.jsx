d84beec44b427125d6756a7ee3e73a9fb1c92d0f
Krys Frost <librarykrystal@gmail.com>
2024-03-26 17:19:46 -0500

    Imports, MUI, and basic setup in DetailsPage.

--- src/components/DetailsPage/DetailsPage.jsx
+++ src/components/DetailsPage/DetailsPage.jsx
@@ -1,5 +1,16 @@
-import React from 'react';
-import {useSelector} from 'react-redux';
+import React, { useState, useEffect, useLayoutEffect } from "react";
+import { useSelector, useDispatch } from 'react-redux';
+import {useHistory} from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
+
+// Material UI Imports
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+import '@fontsource/cabin/400.css';
+import '@fontsource/cabin/700.css';
+import '@fontsource/libre-baskerville';
+import grey from '@mui/material/colors/grey';
+import Button from '@mui/material/Button';
 
 // DETAILS page will include:
 // Full item details-
@@ -8,16 +19,63 @@ import {useSelector} from 'react-redux';
     // star status
     // tag
 // Button to edit (goes to EditItemPage)
-// Button to go bacl home
+// Button to go back home
+
+// Material UI Theming
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Libre Baskerville',
+    ],
+  },
+  palette: {
+    primary: {
+      main: '#d9d9d9',
+    },
+    secondary: {
+      main: grey[700],
+      contrastText: "#fff",
+    },
+    alert: {
+      main: grey[700],
+      contrastText: "#fff",
+    },
+  },
+});
 
 function ItemPage() {
+
   const user = useSelector((store) => store.user);
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const heap = useSelector((store) => store.heap);
+  const tagList = useSelector((store) => store.tag);
+  const item = useSelector((store) => store.item);
+  // const { id } = useParams();
+
+  useEffect(() => {
+    // dispatch({ type: 'FETCH_USER_HEAP' });
+    // dispatch({ type: 'FETCH_ITEM', payload: id });
+    dispatch({ type: 'FETCH_ALL_TAGS' });
+  }, []);
+
+  // Makes each view load scrolled to top
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0)
+  }, []);
+
   return (
-    <div className="container">
-      <h2>Item details page for {user.username}!</h2>
-      <p>Your ID is: {user.id}</p>
-      <p>This is where details of a list item will be.</p>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="container">
+        <h2>Item details page</h2>
+        <Typography sx={{ mb:5 }}>User: {user.username} / ID: {user.id}</Typography>
+        { item ? 
+          <Typography>ITEM #{item.id} HAS ARRIVED</Typography>
+          :
+          <Typography>NO ITEM. HOW SAD.</Typography>
+        }
+      </div>
+    </ThemeProvider>
   );
 }
 
