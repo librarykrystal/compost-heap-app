495bf9ecda05e0335a0bbc04ef101a7650a4655d
Krys Frost <librarykrystal@gmail.com>
2024-03-27 17:50:58 -0500

    Added saga for getting tag by idea ID. Debugged ambiguous user_id in tag query.

--- src/redux/sagas/tag.saga.js
+++ src/redux/sagas/tag.saga.js
@@ -3,7 +3,8 @@ import { put, takeEvery, takeLatest } from 'redux-saga/effects';
 
 // WATCHER SAGA
 function* tagSaga() {
-  yield takeLatest('FETCH_ALL_TAGS', fetchEm);
+  yield takeEvery('FETCH_ALL_TAGS', fetchEm);
+  yield takeEvery('FETCH_TAG', fetchIt);
 }
 
 // WORKER SAGA for GET ALL
@@ -15,6 +16,18 @@ function* fetchEm(action) {
     yield put({ type: 'SET_TAGS', payload: tags.data });
   } catch (error) {
     console.log('ERROR GETTING TAGS:', error);
+  }
+}
+
+// WORKER SAGA for GET ONE by Idea ID
+function* fetchIt(action) {
+  // console.log('Tags GET ALL worker SAGA');
+  try {
+    const tag = yield axios.get(`/api/tag/${action.payload}`);
+    console.log('get ONE TAG SAGA result:', tag.data);
+    yield put({ type: 'SET_TAG', payload: tag.data });
+  } catch (error) {
+    console.log('ERROR GETTING THE TAG:', error);
   }
 }
 
