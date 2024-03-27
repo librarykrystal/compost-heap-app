7100e7a147f7d3ee243fa2f13270217aee2b3996
Krys Frost <librarykrystal@gmail.com>
2024-03-27 17:35:20 -0500

    Added GET (by idea ID) route in tag.router.

--- server/routes/tag.router.js
+++ server/routes/tag.router.js
@@ -9,13 +9,38 @@ router.get('/', (req, res) => {
   console.log('user', req.user);
   // only do GET if authenticated:
   if (req.isAuthenticated()){
-      let queryText = `SELECT * FROM "tag" WHERE "user_id" = $1 ORDER BY "id" ASC`;
-      pool.query(queryText, [req.user.id]).then((result) => {
-          res.send(result.rows);
-      }).catch((error) => {
-          console.log(error);
-          res.sendStatus(500);
-      });
+    let queryText = `SELECT * FROM "tag" WHERE "user_id" = $1 ORDER BY "id" ASC`;
+    pool.query(queryText, [req.user.id]).then((result) => {
+        res.send(result.rows);
+    }).catch((error) => {
+        console.log(error);
+        res.sendStatus(500);
+    });
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+
+// GET TAG by IDEA ID
+router.get('/:id', (req, res) => {
+  console.log('specific tag GET route');
+  console.log('is authenticated?', req.isAuthenticated());
+  console.log('user', req.user);
+  // only do GET if authenticated:
+  if (req.isAuthenticated()){
+    let id = req.params.id;
+    let queryText = `
+      SELECT "tag".label AS "tag_label", "tag".hex AS "tag_hex" FROM "tag"
+      JOIN "idea" ON "idea".tag_id = "tag".id
+      WHERE "idea".id = $1 AND "user_id" = ${req.user.id};
+    `;
+    pool.query(queryText, [id]).then((result) => {
+        res.send(result.rows);
+    }).catch((error) => {
+        console.log(error);
+        res.sendStatus(500);
+    });
   } else {
       res.sendStatus(403);
   }
