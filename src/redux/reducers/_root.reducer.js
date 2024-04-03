17b60e056feefaebea3ad90de3bf65bdf3dc8c5c
Krys Frost <librarykrystal@gmail.com>
2024-04-02 22:14:42 -0500

    Created projects reducer and added to root reducer.

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -2,18 +2,20 @@ import { combineReducers } from 'redux';
 import errors from './errors.reducer';
 import user from './user.reducer';
 import heap from './heap.reducer';
-import tags from './tags.reducer';
+import item from './item.reducer';
+import projects from './projects.reducer';
 import tag from './tag.reducer';
-import item from './item.reducer'
+import tags from './tags.reducer';
 
 
 const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
   user, // will have an id and username if someone is logged in
   heap,
-  tags,
-  tag,
   item,
+  projects,
+  tag,
+  tags,
 });
 
 export default rootReducer;
