a65075f7126804b94782685ac0fc856423228492
Krys Frost <librarykrystal@gmail.com>
2024-04-24 22:06:30 -0500

    Updated GET-BY-ID in tag.router.js to GET by tag ID.)

--- server/routes/tag.router.js
+++ server/routes/tag.router.js
@@ -22,31 +22,26 @@ router.get('/', (req, res) => {
 });
 
 
-// GET TAG by IDEA ID
-// router.get('/:id', (req, res) => {
-//   console.log('specific tag GET route');
-//   console.log('is authenticated?', req.isAuthenticated());
-//   console.log('user', req.user);
-//   console.log('Get TAG by IDEA ID req.params.id:', req.params.id);
-//   // only do GET if authenticated:
-//   if (req.isAuthenticated()){
-    
-//     let id = req.params.id;
-//     let queryText = `
-//       SELECT "tag".label AS "tag_label", "tag".hex AS "tag_hex" FROM "tag"
-//       JOIN "idea" ON "idea".tag_id = "tag".id
-//       WHERE "idea".id = $1 AND "idea"."user_id" = ${req.user.id};
-//     `;
-//     pool.query(queryText, [id]).then((result) => {
-//         res.send(result.rows[0]);
-//     }).catch((error) => {
-//         console.log(error);
-//         res.sendStatus(500);
-//     });
-//   } else {
-//       res.sendStatus(403);
-//   }
-// });
+// GET TAG by tag ID
+router.get('/:id', (req, res) => {
+  console.log('specific tag GET route');
+  console.log('is authenticated?', req.isAuthenticated());
+  console.log('user', req.user);
+  console.log('Get TAG by TAG ID req.params.id:', req.params.id);
+  // only do GET if authenticated:
+  if (req.isAuthenticated()){
+    let id = req.params.id;
+    let queryText = `SELECT * FROM tag WHERE "id" = $1 AND "user_id" = ${req.user.id};`;
+    pool.query(queryText, [id]).then((result) => {
+        res.send(result.rows[0]);
+    }).catch((error) => {
+        console.log(error);
+        res.sendStatus(500);
+    });
+  } else {
+      res.sendStatus(403);
+  }
+});
 
 
 // POST NEW TAG
